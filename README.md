433a287bc6b603a2ce6fe7974bbbaa51ac148e27
zhmhbest <zhmhbest@gmail.com>
2020-03-08 18:25:26 +0800

    基础登录功能

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FlaskVueElementAdmin
+
 后台管理项目解决方案
+
+- 前端：[Vue-Element-Admin](https://panjiachen.github.io/vue-element-admin-site/zh/guide/)
+- 后端：Python3.6.8 + Flask + SQLAlchemy
--- View/src/router/index.js
+++ View/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
 import Layout from '@/layout'
@@ -89,7 +89,8 @@ export const constantRoutes = [
       }
     ]
   }
-]
+];
+
 
 /**
  * asyncRoutes
@@ -154,32 +155,21 @@ export const asyncRoutes = [
     ]
   },
 
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
-]
+];
 
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
-})
+});
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
+  const newRouter = createRouter();
   router.matcher = newRouter.matcher // reset router
 }
 
